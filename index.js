f0162e9e54336134ef7e231e4ac48bc1bfb68a5b
Illbeblack <shvartsdenis@mail.ru>
2023-06-08 12:51:43 +0300

    feat: define html elements in js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//   Elemets selection
+const score1 = document.getElementById('score--0');
+const score2 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+//   Game initial conditions
+score1.textContent = 0;
+score2.textContent = 0;
+dice.classList.add('hidden');
